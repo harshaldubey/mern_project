fe5eef9d5f5fb9bb5d79b095d19a6ed9a9c85d6d
Harshal Dubey <68217194+harshaldubey@users.noreply.github.com>
2021-07-09 22:34:40 +0530

    mern_project_first_commit

---
+++ server/app.js
@@ -0,0 +1,42 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+
+dotenv.config({ path: "./config.env" });
+
+require("./db/conn");
+// const User = require("./model/userSchema");
+
+app.use(express.json());
+
+// we link the router files to make route easy
+app.use(require("./router/auth"));
+
+const PORT = process.env.PORT;
+
+// Middelware
+const Middelware = (req, res, next) => {
+  console.log("Hello my middleware");
+  next();
+};
+
+// app.get("/", (req, res) => {
+//   res.send(`Hello world from the server app.js`);
+// });
+app.get("/about", Middelware, (req, res) => {
+  res.send(`Hello About world from the server`);
+});
+app.get("/signin", (req, res) => {
+  res.send(`Hello Login world from the server`);
+});
+app.get("/signup", (req, res) => {
+  res.send(`Hello Registration world from the server`);
+});
+app.get("/contact", (req, res) => {
+  res.send(`Hello Contact world from the server`);
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
---
+++ server/router/auth.js
@@ -0,0 +1,106 @@
+const jwt = require("jsonwebtoken");
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+require("../db/conn");
+const User = require("../model/userSchema");
+
+router.get("/", (req, res) => {
+  res.send(`Hello world from the server router js`);
+});
+
+// using promises
+// router.post("/register", (req, res) => {
+//   const { name, email, phone, work, password, cpassword } = req.body;
+
+//   if (!name || !email || !phone || !work || !password || !cpassword) {
+//     res.status(422).json({ error: "Plz filled the field properly" });
+//   }
+
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(422).json({ error: "Email already exist" });
+//       }
+
+//       const user = new User({ name, email, phone, work, password, cpassword });
+
+//       user
+//         .save()
+//         .then(() => {
+//           res.status(201).json({ message: "user registered successfully" });
+//         })
+//         .catch((err) => res.status(500).json({ error: "Failed to register" }));
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+// Async-Await
+router.post("/register", async (req, res) => {
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({ error: "Plz fill the field properly" });
+  }
+
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "Email already exist" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ error: " password are not matching" });
+    } else {
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      await user.save();
+
+      res.status(201).json({ message: "user registered successfully" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// login route
+
+router.post("/signin", async (req, res) => {
+  try {
+    let token;
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: "Plz Filled the data " });
+    }
+
+    const userLogin = await User.findOne({ email: email });
+
+    // console.log(userLogin);
+
+    if (userLogin) {
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+
+      token = await userLogin.generateAuthToken();
+      console.log(token);
+
+      res.cookie("jwtoken", token, {
+        expires: new Date(Date.now() + 2589200000),
+        htttpOnly: true,
+      });
+
+      if (!isMatch) {
+        res.status(400).json({ error: "Invalid Cradentials Pass" });
+      } else {
+        res.json({ message: "user Signin successfully" });
+      }
+    } else {
+      res.status(400).json({ error: "Inavlid Cradentials" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
