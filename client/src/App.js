369a6dc5adc49409f297e5d2f08da95513e45e37
Harshal Dubey <68217194+harshaldubey@users.noreply.github.com>
2021-07-21 15:51:32 +0530

    logout term added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React, { createContext, useReducer } from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
-import { Route, Switch } from "react-router-dom";
-
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -11,36 +10,48 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Errorpage from "./components/Errorpage";
 import Logout from "./components/Logout";
+import { initialState, reducer } from "../src/reducer/UseReducer";
 
+// context API
+export const UserContext = createContext();
+
+const Routing = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route path="/logout">
+        <Logout />
+      </Route>
+      <Route>
+        <Errorpage />
+      </Route>
+    </Switch>
+  );
+};
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div>
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/logout">
-          <Logout />
-        </Route>
-        <Route>
-          <Errorpage />
-        </Route>
-      </Switch>
+      <UserContext.provider value={{ state, dispatch }}>
+        <Navbar />
+        <Routing />
+      </UserContext.provider>
     </div>
   );
 };
-
 export default App;
