68585b57986d5a049b603d74fb67995d2ae627a5
Harshal Dubey <68217194+harshaldubey@users.noreply.github.com>
2021-07-22 22:21:47 +0530

    hosting the Project

--- server/app.js
+++ server/app.js
@@ -15,7 +15,8 @@ app.use(express.json());
 // we link the router files to make route easy
 app.use(require("./router/auth"));
 
-const PORT = process.env.PORT;
+//2:step heroku
+const PORT = process.env.PORT || 5000;
 
 // Middelware
 // const Middelware = (req, res, next) => {
@@ -35,6 +36,12 @@ const PORT = process.env.PORT;
 app.get("/signup", (req, res) => {
   res.send(`Hello Registration world from the server`);
 });
+
+// 3:step heroku
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+}
+
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
 });
